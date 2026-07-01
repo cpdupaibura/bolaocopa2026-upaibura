430d00729de8d7d3547be92336f47c96f2345640
Enio Batalha <enio@upaibr.local>
2026-07-01 08:50:20 -0300

    mexico 2 x 0 equador

--- src/data/knockout_results.ts
+++ src/data/knockout_results.ts
@@ -10,4 +10,5 @@ export const KNOCKOUT_RESULTS: Record<string, 'a' | 'b'> = {
   'R16-9': 'a', // Brasil 2x1 Japão
   'R16-10': 'b', // Noruega 2x1 Costa do Marfim
   'R16-2': 'a',  // França 3x0 Suécia
+  'R16-11': 'a',  // México 2x0 Equador
 };
